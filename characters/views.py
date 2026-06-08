5555d3793f3e129a9ee1b61d7a1d513cb9a957a7
Cody Henry <60040485+codyhenry@users.noreply.github.com>
2026-06-08 10:54:12 -0400

    Import Q for validation dashboard filters

--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
